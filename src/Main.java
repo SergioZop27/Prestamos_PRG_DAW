4a90f83cc365ab440b228465cb0958c06dc73510
sergio <sergio.lopsan.9@educajcyl.es>
2026-02-10 11:10:46 +0100

    Acabando usuario

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+//        Usuario user1 = new Usuario("pepe","a@a.com",);
+
+
+
+
+    }
+}
--- src/Usuario.java
+++ src/Usuario.java
@@ -10,34 +10,57 @@ public class Usuario {
 
 
 
-    public Usuario(String nombre, String email,String numeroSocio,LocalDate fechaRegistro){
-        this.nombre=nombre;
-        this.fechaRegistro=fechaRegistro;
+    public Usuario(String nombre, String email,String numeroSocio,LocalDate fechaRegistro) throws Exception{
+        try {
+            this.nombre = nombre;
+            this.fechaRegistro = fechaRegistro;
 
-        boolean formatoEmail = email.matches("\\w{1,}@\\w{1,}\\.\\w{1,}");
-        boolean formatoSocio = numeroSocio.matches("^SOC[0-9]{5}");
+            boolean formatoEmail = email.matches("\\w{1,}@\\w{1,}\\.\\w{1,}");
+            boolean formatoSocio = numeroSocio.matches("^SOC[0-9]{5}");
 
-        //Condicion Email
-        if(formatoEmail) {
-            this.email=email;
-        }else{
-            //incorrecto excepción de tipo UsuarioInvalidoException COMPLETAR
-        }
+            //Condicion Email
+            if (formatoEmail) {
+                this.email = email;
+            } else {
+                throw new Exception("Email incorrecto tiene que ser este formato: *@*.*");
+            }
 
-        //Condicion Num Socio
-        if(formatoSocio) {
-            this.numeroSocio=numeroSocio;
-        }else{
-            //incorrecto excepción de tipo UsuarioInvalidoException COMPLETAR
-        }
+            //Condicion Num Socio
+            if (formatoSocio) {
+                this.numeroSocio = numeroSocio;
+            } else {
+                throw new Exception("Numero de socio incorrecto tiene que ser este formato: SOC00000");
+            }
+        } catch (Exception UsuarioInvalidoException) {
+            System.out.println("Error usuario invalido, el nombre solo puede tener letras");
+       }
+
+
+
 
     }
 
-//    public void sancionar(){
-//        this.fechaFinSancion
-//        LocalDate.now();
-//
-//    }
+    public void sancionar(){
 
+
+
+    }
+   public void levantarSancion(){
+        this.sancionado=false;
+        this.fechaFinSancion = null;
+   }
+   public boolean estaSancionado(){
+        return sancionado;
+   }
+
+   @Override
+   public String toString(){
+        return "Nombre: "+nombre+
+                "Email: " +email+
+                "Num Socio: " +numeroSocio+
+                "fech Registro: "+fechaRegistro+
+                "sancionado: "+sancionado+
+                "fech fin sancion: "+ fechaFinSancion;
+    }
 
 }
